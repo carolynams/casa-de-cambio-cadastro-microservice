50215b37be971462f3e177a72aacbe837f62d5b7
Carolyna <Carolynacaarolmantovani@hotmail.com>
2020-11-06 10:13:34 -0300

    added project

---
+++ src/main/java/com/springspecialistalgaworks/SpringSpecialistAlgaworksApplication.java
@@ -0,0 +1,13 @@
+package com.springspecialistalgaworks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSpecialistAlgaworksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSpecialistAlgaworksApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springspecialistalgaworks/SpringSpecialistAlgaworksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springspecialistalgaworks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSpecialistAlgaworksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
