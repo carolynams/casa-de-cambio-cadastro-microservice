d3f1ed8033b648a92b96f669ed0fc132c8e7df1b
Carolyna <Carolynacaarolmantovani@hotmail.com>
2020-11-23 09:50:53 -0300

    cleaning test project, started a new project with datatbase connection

--- src/main/java/com/springspecialistalgaworks/notification/Notificador.java
+++
@@ -1,9 +0,0 @@
-package com.springspecialistalgaworks.notification;
-
-import com.springspecialistalgaworks.model.Cliente;
-
-public interface Notificador {
-
-    void notificar(Cliente cliente, String mensagem);
-
-}
